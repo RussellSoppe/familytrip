7bb8052ca2f51d9c35fc022b0afc368ad8a0d037
russellicious <42648750+russellicious@users.noreply.github.com>
2021-06-08 19:00:42 -0500

    Updates

---
+++ precache-manifest.12ca415ec63ae5cee30615880503454b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc9d3434a4f8eb7fb1df19052273d743",
+    "url": "/familytrip/index.html"
+  },
+  {
+    "revision": "b1314bbaae84f04b1405",
+    "url": "/familytrip/static/css/main.b35bd2ad.chunk.css"
+  },
+  {
+    "revision": "6a137860593f3f9e60d3",
+    "url": "/familytrip/static/js/2.4ad07c59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/familytrip/static/js/2.4ad07c59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1314bbaae84f04b1405",
+    "url": "/familytrip/static/js/main.9f9968fa.chunk.js"
+  },
+  {
+    "revision": "833b05552d4cc5206327",
+    "url": "/familytrip/static/js/runtime-main.a493def9.js"
+  },
+  {
+    "revision": "b68f3f2b1744b9cb09b9b5ca5ae20709",
+    "url": "/familytrip/static/media/Flat Classic Car Vectors-01.b68f3f2b.svg"
+  },
+  {
+    "revision": "e93e1dd25fd8727c53c0b8ce42cb0e32",
+    "url": "/familytrip/static/media/chicago.e93e1dd2.jpg"
+  },
+  {
+    "revision": "e4daeda1e125f00a67fd647eca049ee0",
+    "url": "/familytrip/static/media/grandcanyon.e4daeda1.jpg"
+  },
+  {
+    "revision": "913ec68c2b4a5dccdfbf4eb91270ed92",
+    "url": "/familytrip/static/media/logo.913ec68c.jpg"
+  },
+  {
+    "revision": "ca9d1e7e1dfc003e00e88f0649d24f79",
+    "url": "/familytrip/static/media/memphis.ca9d1e7e.jpg"
+  },
+  {
+    "revision": "f7a434610d38e69dfa4273c3a2382115",
+    "url": "/familytrip/static/media/minneapolis.f7a43461.jpg"
+  },
+  {
+    "revision": "b46a0dbd730c60ad801648c3cc742108",
+    "url": "/familytrip/static/media/mtrushmore.b46a0dbd.jpg"
+  },
+  {
+    "revision": "3ed848ec3fce37cefc3ae31eff7be99c",
+    "url": "/familytrip/static/media/police_icon.3ed848ec.png"
+  },
+  {
+    "revision": "7f79b3f454558677a1028d5a014904b2",
+    "url": "/familytrip/static/media/software_dev_icon.7f79b3f4.png"
+  },
+  {
+    "revision": "a7225ab3f9276140a9e52914d2ddc927",
+    "url": "/familytrip/static/media/teacher_icon.a7225ab3.png"
+  }
+]);
--- src/gamedata/badevents.js
+++
@@ -1,9 +0,0 @@
-
-const badevents = {
-	0:{description:"Player gets sick, uses 1 barf bag", inventorycat: "myBarfBags", cost: -1}, 
-	1:{description:"Player eats while bored, uses 2 food supply", inventorycat: "myFood", cost: -2},
-	2:{description:"Player gets robbed, loses $50", inventorycat: "myMoney", cost: -50},
-	3:{description:"Player gets flat tire, lose 1 hour of travel", inventorycat: "myGas", cost: -1}
-};
-
-export default badevents;
